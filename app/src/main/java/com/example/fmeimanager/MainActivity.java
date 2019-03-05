9bd89370feeb7272ccff0e4bd95d2708ba13cf19
francois LE FORT <lefort.francois_pro@yahoo.com>
2019-03-05 15:48:55 +0100

    layouts organisation root definition ; primary colors definitions

--- app/src/main/java/com/example/fmeimanager/MainActivity.java
+++ app/src/main/java/com/example/fmeimanager/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.fmeimanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.fmeimanager.controllers.activities.FmeiDashboard;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton mImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageButton = findViewById(R.id.run_touch_screen);
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startFirstActivity();
+            }
+        });
+    }
+
+    private void startFirstActivity(){
+        startActivity(new Intent(this, FmeiDashboard.class));
     }
 }
---
+++ app/src/main/java/com/example/fmeimanager/controllers/activities/FmeiDashboard.java
@@ -0,0 +1,15 @@
+package com.example.fmeimanager.controllers.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fmeimanager.R;
+
+public class FmeiDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fmei_dashboard);
+    }
+}
