2826b7951c27179c64ae2e178f0f8ad21ab6d611
francois LE FORT <lefort.francois_pro@yahoo.com>
2019-05-22 15:52:36 +0200

    about section created

--- app/src/main/java/com/example/fmeimanager/controllers/navigationPackageE/AboutActivity.java
+++ app/src/main/java/com/example/fmeimanager/controllers/navigationPackageE/AboutActivity.java
@@ -1,8 +1,13 @@
 package com.example.fmeimanager.controllers.navigationPackageE;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.fmeimanager.R;
@@ -12,32 +17,14 @@ import com.example.fmeimanager.injection.ViewModelFactory;
 import com.example.fmeimanager.database.Participant;
 import com.example.fmeimanager.viewmodels.ParticipantViewModel;
 
-public class AboutActivity extends BaseActivity implements AboutFragment.AboutListener{
+public class AboutActivity extends AppCompatActivity{
 
     @Override
-    protected Fragment getFirstFragment() {
-        return AboutFragment.newInstance();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_about);
+        Button close = findViewById(R.id.about_btn);
+        close.setOnClickListener(v -> finish());
     }
-
-    @Override
-    protected int getContentView() {
-        return R.layout.activity_about;
-    }
-
-    @Override
-    protected int getFragmentLayout() {
-        return R.id.frame_layout_about;
-    }
-
-    @Override
-    protected boolean isAChildActivity() {
-        return false;
-    }
-
-
-    @Override
-    public void updateAboutNavHeader(Participant participant) {
-        this.updateHeader(participant);
-    }
-
 }
